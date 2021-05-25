550671b69aac1c174de23302e6370ac1729b76d6
EmmanuelGutierrez3877 <emmanuel.gutierrez3877@alumnos.udg.mx>
2021-05-25 14:04:57 -0500

    move php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+</head>
+
+<body>
+
+
+    <?php 
+        ini_set('max_execution_time',0);
+        $conex = mysqli_connect("localhost","root","","Camara");
+
+        while(1==1){
+            $consulta = "SELECT * FROM `imagenes` ORDER BY `imagenes`.`fecha` DESC";
+            $resultado = mysqli_query($conex,$consulta);
+            $foto = $resultado->fetch_assoc();
+        
+    ?>
+
+
+
+    <div id="foto" class="col-xs-11 col-sm-11 col-lg-7">
+        <?php echo($foto['fecha']) ?>
+        <?php 
+                echo ('<img style="max-height: 100%; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $foto['imagen'] ).'"/>');
+            ?>
+    </div>
+
+
+    <?php 
+            while(1==1){
+                sleep(0.5);
+                $resultado2 = mysqli_query($conex,$consulta);
+                $foto2 = $resultado2->fetch_assoc();
+
+                if($foto2['id']!=$foto['id']){
+                    $resultado = $resultado2;
+                    ?>
+                    <script>
+                        var b = document.getElementById("foto");
+                        b.parentNode.removeChild(b);
+                    </script>
+                    <?php
+                    break;
+                }
+                
+            }
+    
+        }
+    
+    ?>
+
+</body>
+
+</html>
