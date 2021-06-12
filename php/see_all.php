23219fff977af2add7359d32e28ec9da2c6f26a9
EmmanuelGutierrez3877 <emmanuel.gutierrez3877@alumnos.udg.mx>
2021-06-11 20:04:12 -0500

    see_all

--- php/see_all.php
+++ php/see_all.php
@@ -21,7 +21,6 @@
         </table>
         </div>
     <br>
-
     <?php 
 	    //echo phpversion();
         ini_set('max_execution_time',0);
@@ -29,11 +28,10 @@
 	    //$conex = mysqli_connect("localhost","root","","Camara");
         $conex = mysqli_connect("sql10.freemysqlhosting.net","sql10414886","ezeL3RqZWt","sql10414886");
 
-        while(1==1){
-            $consulta = "SELECT * FROM `imagenes` ORDER BY `imagenes`.`fecha` DESC";
-            $resultado = mysqli_query($conex,$consulta);
-            $foto = $resultado->fetch_assoc();
         
+        $consulta = "SELECT * FROM `imagenes` ORDER BY `imagenes`.`fecha` DESC";
+        $resultado = mysqli_query($conex,$consulta);
+        while($foto = $resultado->fetch_assoc()){
     ?>
 
     <div id="foto" class="col-xs-11 col-sm-11 col-lg-7 foto">
@@ -42,30 +40,8 @@
             echo ('<img style="max-height: 100%; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $foto['imagen'] ).'"/>');
         ?>
     </div>
-
-
-    <?php 
-            while(1==1){
-                sleep(0.5);
-                $resultado2 = mysqli_query($conex,$consulta);
-                $foto2 = $resultado2->fetch_assoc();
-
-                if($foto2['id']!=$foto['id']){
-                    $resultado = $resultado2;
-                    ?>
-                    <script>
-                        var b = document.getElementById("foto");
-                        b.parentNode.removeChild(b);
-                    </script>
-                    <?php
-                    break;
-                }
-                
-            }
-    
-        }
-    
-    ?>
+        <br>
+        <?php } ?>
 
 </body>
 
